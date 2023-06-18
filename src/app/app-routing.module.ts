44a89f5198c3a45f3cc0209006928abe195fd598
Mauricio Huisha <lojahuishita@gmail.com>
2023-06-18 18:37:41 -0500

    Menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,16 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-
 const routes: Routes = [
-  
- 
-  {
-    path: '',
-    redirectTo: 'inicio',
-    pathMatch: 'full'
-  },
-  {
-    path: 'inicio',
-    loadChildren: () => import('./pages/inicio/inicio.module').then( m => m.InicioPageModule)
-  },
+{
+path: 'inicio',
+loadChildren: () => import('./pages/inicio/inicio.module').then(m =>
+m.InicioPageModule)
+},
+{
+path: '',
+redirectTo: 'inicio',
+pathMatch: 'full'
+},
   {
     path: 'alert',
     loadChildren: () => import('./pages/alert/alert.module').then( m => m.AlertPageModule)
@@ -22,22 +20,15 @@ const routes: Routes = [
     loadChildren: () => import('./pages/action-sheet/action-sheet.module').then( m => m.ActionSheetPageModule)
   },
   {
-    path: 'avatar',
-    loadChildren: () => import('./pages/avatar/avatar.module').then( m => m.AvatarPageModule)
+    path: 'input',
+    loadChildren: () => import('./pages/input/input.module').then( m => m.InputPageModule)
   },
-  {
-    path: 'personalizado',
-    loadChildren: () => import('./pages/personalizado/personalizado.module').then( m => m.PersonalizadoPageModule)
-  },
-
-  
-  
 ];
-
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
-  exports: [RouterModule]
+imports: [
+RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules
+})
+],
+exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/pages/input/input-routing.module.ts
+++ src/app/pages/input/input-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { AvatarPage } from './avatar.page';
+import { InputPage } from './input.page';
 
 const routes: Routes = [
   {
     path: '',
-    component: AvatarPage
+    component: InputPage
   }
 ];
 
@@ -14,4 +14,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class AvatarPageRoutingModule {}
+export class InputPageRoutingModule {}
